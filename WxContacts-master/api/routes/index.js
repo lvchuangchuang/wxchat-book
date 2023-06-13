b893fcbb7d07dd4c1bcf779f0bd8a9dd30087211
lvchuangchuang <112074994+lvchuangchuang@users.noreply.github.com>
2023-06-13 14:57:55 +0800

    Delete WxContacts-master directory

--- WxContacts-master/api/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router =  require('koa-router')();
-const contact = require('../contact/index.js');
-
-
-router.use('/contact', contact.routes(), contact.allowedMethods());
-
-module.exports = router;
